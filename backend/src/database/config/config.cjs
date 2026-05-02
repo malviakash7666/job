8f691d4f7f80d99b56b87f1e77931675d0571681
Akash Ajay Malvi <akashmalvi@gmail.com>
2026-05-02 16:15:20 +0530

    fix config

--- backend/src/database/config/config.cjs
+++ backend/src/database/config/config.cjs
@@ -20,12 +20,19 @@ module.exports = {
     logging: false,
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    dialect: "postgres",
-    logging: false,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  dialect: "postgres",
+  logging: false,
+
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
   },
+},
 };
